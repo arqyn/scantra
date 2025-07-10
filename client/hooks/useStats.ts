fb336df070b43b473aaf922fa7c8cb7ed48b5d6e
Saeed Halabi <saeedhalabi80@gmail.com>
2025-07-10 11:24:00 +0300

    style: add braces to if statement in `useStats` for consistency
    Aligned if statement formatting with project conventions to improve readability.

--- client/hooks/useStats.ts
+++ client/hooks/useStats.ts
@@ -3,6 +3,8 @@ import { StatsContext } from "@/context/StatsProvider";
 
 export const useStats = () => {
   const context = useContext(StatsContext);
-  if (!context) throw new Error("useStats must be used within a StatsProvider");
+  if (!context) {
+    throw new Error("useStats must be within a StatsProvider");
+  }
   return context;
 };
