dd1af60263d36af5011a10f3df88f535192ff1e7
Saeed Halabi <104928197+saeedhalabi@users.noreply.github.com>
2025-08-19 22:45:16 +0300

    Update corsMiddleware.ts

--- server/src/middleware/corsMiddleware.ts
+++ server/src/middleware/corsMiddleware.ts
@@ -1,6 +1,7 @@
 import cors, { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
+  // TODO: Add deployed site URL to allowed origins once deployed
   origin: "http://localhost:8081",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
